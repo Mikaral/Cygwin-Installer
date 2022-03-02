1c0ec2bcb7dec91260d43b88b335e010cd0e78d5
Mikael Amaral <55303570+Mikaral@users.noreply.github.com>
2022-03-02 04:01:19 -0300

    Fisrt Commit

---
+++ install.bat
@@ -0,0 +1,37 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+start /WAIT %~dp0/setup-x86_64.exe
+
+setx /M PATH "%PATH%;C:\cygwin64\bin"
+
+IF %ERRORLEVEL% NEQ 0 (
+  ECHO Houve um problema no procedimento, favor reclamar com Mikael por sua incopetencia
+) ELSE (
+  ECHO Compilador instalado e adicionado a PATH com sucesso =D
+)
+ 
+pause
