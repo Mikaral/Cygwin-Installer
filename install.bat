dc1eb95b0aaf344bdc9e561f755c63a0541f1053
Mikael Amaral <55303570+Mikaral@users.noreply.github.com>
2022-03-02 17:33:00 -0300

    Removed setup and improved the batch installer

--- install.bat
+++ install.bat
@@ -1,5 +1,9 @@
 @echo off
 
+REM -- Automates cygwin installation
+REM -- Source: https://github.com/Mikaral/Cygwin-Installer
+REM -- Based on: https://github.com/rtwolf/cygwin-auto-install
+
 :: BatchGotAdmin
 :-------------------------------------
 REM  --> Check for permissions
@@ -24,14 +28,52 @@ if '%errorlevel%' NEQ '0' (
     CD /D "%~dp0"
 :--------------------------------------
 
-start /WAIT %~dp0/setup-x86_64.exe
+SETLOCAL
+ 
+REM -- Change to the directory of the executing batch file
+CD %~dp0
 
-setx /M PATH "%PATH%;C:\cygwin64\bin"
+REM -- Download the Cygwin installer
+IF NOT EXIST cygwin-setup.exe (
+	ECHO cygwin-setup.exe NOT found! Downloading installer...
+	bitsadmin /transfer cygwinDownloadJob /download /priority normal https://cygwin.com/setup-x86_64.exe %CD%\\cygwin-setup.exe
+) ELSE (
+	ECHO cygwin-setup.exe found! Skipping installer download...
+)
+ 
+REM -- Configure our paths
+SET SITE=https://linorg.usp.br/cygwin/
+SET LOCALDIR=%CD%
+SET ROOTDIR=C:\cygwin
+ 
+REM -- These are the packages we will install (in addition to the default packages)
+SET PACKAGES=gcc-core,gcc-g++
+ 
+REM -- More info on command line options at: https://cygwin.com/faq/faq.html#faq.setup.cli
+REM -- Do it!
+ECHO *** INSTALLING DEFAULT PACKAGES
+cygwin-setup --quiet-mode --no-desktop --download --local-install --no-verify -s %SITE% -l "%LOCALDIR%" -R "%ROOTDIR%"
+ECHO.
+ECHO.
+ECHO *** INSTALLING CUSTOM PACKAGES
+cygwin-setup -q -d -D -L -X -s %SITE% -l "%LOCALDIR%" -R "%ROOTDIR%" -P %PACKAGES%
+ 
+REM -- Show what we did
+ECHO.
+ECHO.
+ECHO cygwin installation updated
+ECHO  - %PACKAGES%
+ECHO.
+
+setx /M PATH "%PATH%;%ROOTDIR%\bin"
 
 IF %ERRORLEVEL% NEQ 0 (
   ECHO Houve um problema no procedimento, favor reclamar com Mikael por sua incopetencia
 ) ELSE (
   ECHO Compilador instalado e adicionado a PATH com sucesso =D
 )
+
+ENDLOCAL
  
-pause
+PAUSE
+EXIT /B 0
